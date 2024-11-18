817de1edd6ed51313846fab3257eff4a2b42d7b1
Philipp Jurasic <philipp.jurasic@tum.de>
2024-11-18 03:26:40 +0100

    build, but probably wont run

--- setup.py
+++ setup.py
@@ -4,12 +4,10 @@ import os # environ
 import sys
 import json
 import warnings
-import argparse
-import setuptools
 
-from os.path import basename, splitext
-from glob import glob
 import numpy
+import subprocess
+from skbuild import setup
 
 print("system.platform is {}".format(sys.platform))
 if (sys.platform == "darwin"):
@@ -17,8 +15,6 @@ if (sys.platform == "darwin"):
     vars = sysconfig.get_config_vars()
     vars['LDSHARED'] = vars['LDSHARED'].replace('-bundle', '-dynamiclib')
 
-from setuptools import find_packages
-from skbuild import setup
 
 # Load machine-specific options from cmake_config.json.
 # It should contain a dict with an array called cmake_args.
@@ -37,6 +33,8 @@ if 'CMAKE_ARGS' in os.environ:
 # see also: https://stackoverflow.com/a/14657667
 d['cmake_args'].append("-DCMAKE_C_FLAGS=-I"+numpy.get_include())
 
+d['cmake_args'].append('-DSKBUILD=ON')
+
 # Unset conflicting environment variables temporarily. They are restored on exit.
 conflicting_env_vars = ["HDF5", "HDF5_ROOT", "HDF5_HOME", "FFTW", "FFTW_DIR"]
 for env_var in conflicting_env_vars:
@@ -44,7 +42,6 @@ for env_var in conflicting_env_vars:
     print("Detected potentially conflicting envirnoment variable", env_var, "temporarily unset for the installation.")
 
 # Symlink f2py to f2py3, if needed (if there isn't a conda installed version)
-import subprocess
 f2py_path = subprocess.getoutput("which f2py3")
 conda_env_path = os.environ.get("CONDA_PREFIX", None)
 if conda_env_path is None:
@@ -55,24 +52,16 @@ else:
         if not os.path.exists(f2py_path):
             raise ImportError("Couldn't find f2py, which is required for the build. ") 
         f2py3_path = os.path.join(conda_env_path,"bin/f2py3")
-        print("Couldn't find f2py3, attempting to symlink ", f2py_path, "to", f2py3_path)
         os.symlink(f2py_path, f2py3_path)
+        print("Couldn't find f2py3, created a symlink ", f2py_path, "to", f2py3_path)
 
+import setuptools
 
-setup(
+setuptools.setup(
     name="spec",
-    version="0.0.3",
-    #license="MIT",
     packages=['spec'],
     package_dir={'': 'Utilities/python_wrapper'},
-    #py_modules=[splitext(basename(path))[0] for path in glob('src/vmec/*.py')],
-    install_requires=['f90wrap', 'scikit-build'],
-    classifiers=[
-        "Development Status :: 1 - Alpha",
-        "Intended Audience :: Nuclear Fusion Community",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Topic :: MHD Equilibrium Solver"],
     cmake_args=d['cmake_args'],
+    cmake_install_dir=os.path.join(".", "install")
+
 )
